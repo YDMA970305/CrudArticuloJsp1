1872b5df72d021d99e006634255527dc88423c2d
YDMA970305 <ymartineza@unicartagena.edu.co>
2025-05-19 17:58:15 -0500

    Creacion de la estructura

---
+++ src/java/Libs/DatabaseConfig.java
@@ -0,0 +1,10 @@
+package Libs;
+
+public class DatabaseConfig {
+
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/your_database_name";
+    public static final String USER = "your_username";
+    public static final String PASSWORD = "your_password";
+    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    
+} 
